e776169fafde4b82439dfad2b2643bbd9466e906
Matt Fellenz <matt@felle.nz>
2023-10-02 14:20:53 -0700

    Implement editing and deleting problems

--- src/routes/problems/new.rs
+++ src/routes/problems/new.rs
@@ -17,19 +17,22 @@ use crate::util::deserialize_textarea;
 use crate::State;
 
 #[derive(Debug, serde::Deserialize)]
-struct Form {
-	title: String,
+pub struct Problem {
+	pub name: String,
 	#[serde(deserialize_with = "deserialize_textarea")]
-	description: String,
+	pub description: String,
 
-	time_limit: u32,
-	memory_limit: u32,
+	pub time_limit: u32,
+	pub memory_limit: u32,
 	#[serde(default)]
-	visible: bool,
+	pub visible: bool,
 
 	#[serde(deserialize_with = "deserialize_textarea")]
-	tests: String,
+	pub tests: String,
 }
+
+const DEFAULT_TIME_LIMIT: u32 = 1000;
+const DEFAULT_MEMORY_LIMIT: u32 = 8;
 
 const EXAMPLE_TESTS: &str = "\
 first input
@@ -41,17 +44,58 @@ second input
 second output
 ";
 
-async fn handle_post(state: &State, user: &User, post: &Form) -> Result<ProblemId, ErrorResponse> {
+async fn handle_post(
+	state: &State,
+	user: &User,
+	post: &Problem,
+) -> Result<ProblemId, ErrorResponse> {
 	Tests::validate(&post.tests).map_err(|error| ErrorResponse::bad_request(error.to_string()))?;
 	let now = now();
-	let id = query_scalar!("insert into problems (name, description, time_limit, memory_limit, visible, tests, creation_time, created_by) values (?, ?, ?, ?, ?, ?, ?, ?) returning id", post.title, post.description, post.time_limit, post.memory_limit, post.visible, post.tests, now, user.id).fetch_one(&state.database).await.map_err(ErrorResponse::internal)?;
+	let id = query_scalar!("insert into problems (name, description, time_limit, memory_limit, visible, tests, creation_time, created_by) values (?, ?, ?, ?, ?, ?, ?, ?) returning id", post.name, post.description, post.time_limit, post.memory_limit, post.visible, post.tests, now, user.id).fetch_one(&state.database).await.map_err(ErrorResponse::internal)?;
 	Ok(id)
+}
+
+pub fn problem_form(old: Option<&Problem>) -> maud::Markup {
+	html! {
+		label {
+			"Name"
+			input type="text" required name="name" value=[old.map(|post| &post.name)] {}
+		}
+		label {
+			"Description"
+			textarea required name="description" rows="4" { (old.map_or("", |post| &post.description)) }
+		}
+		label {
+			"Time limit (milliseconds)"
+			input type="number" required name="time_limit" value=(old.map_or(DEFAULT_TIME_LIMIT, |post| post.time_limit));
+		}
+		label {
+			"Memory limit (MB)"
+			input type="number" required name="memory_limit" value=(old.map_or(DEFAULT_MEMORY_LIMIT, |post| post.memory_limit));
+		}
+		label {
+			"Visible"
+			input type="checkbox" name="visible" value="true" checked[old.map_or(true, |post| post.visible)];
+		}
+		label {
+			"Tests"
+			textarea required name="tests" placeholder=(EXAMPLE_TESTS) rows="15" { (old.map_or("", |post| post.tests.trim())) }
+			details {
+				summary { "How to write tests" }
+				p { "Separate test cases with three equals signs on their own line." }
+				p { "Separate input and output in a test case with two dashes on their own line." }
+				p { "Example:"}
+				pre { code { (EXAMPLE_TESTS) } }
+				p { "The first test case will be shown as an example on the problem page." }
+			}
+		}
+	}
 }
 
 async fn handler(
 	extract::State(state): extract::State<Arc<State>>,
 	ProblemAuthor(user): ProblemAuthor,
-	IfPost(post): IfPost<extract::Form<Form>>,
+	IfPost(post): IfPost<extract::Form<Problem>>,
 ) -> Response {
 	let post = post.map(|extract::Form(post)| post);
 	let post = post.as_ref();
@@ -67,37 +111,7 @@ async fn handler(
 
 	let body = html! {
 		form method="post" {
-			label {
-				"Title"
-				input type="text" required name="title" value=[post.map(|post| &post.title)] {}
-			}
-			label {
-				"Description"
-				textarea required name="description" rows="4" { (post.map_or("", |post| &post.description)) }
-			}
-			label {
-				"Time limit (milliseconds)"
-				input type="number" required name="time_limit" value=[post.map(|post| &post.time_limit)];
-			}
-			label {
-				"Memory limit (MB)"
-				input type="number" required name="memory_limit" value=[post.map(|post| &post.memory_limit)];
-			}
-			label {
-				"Visible"
-				input type="checkbox" name="visible" value="true" checked[post.map_or(true, |post| post.visible)];
-			}
-			label {
-				"Tests"
-				textarea required name="tests" placeholder=(EXAMPLE_TESTS) rows="10" { (post.map_or("", |post| &post.tests)) }
-			}
-			details {
-				summary { "How to write tests" }
-				p { "Separate test cases with three equals signs on their own line." }
-				p { "Separate input and output in a test case with two dashes on their own line." }
-				p { "Example:"}
-				pre { code { (EXAMPLE_TESTS) } }
-			}
+			(problem_form(post))
 			input type="submit" value="Create";
 		}
 	};
--- src/routes/problems/problem.rs
+++ src/routes/problems/problem.rs
@@ -2,34 +2,128 @@ use std::sync::Arc;
 
 use axum::extract;
 use axum::http::StatusCode;
-use axum::response::{IntoResponse, Response};
-use axum::routing::get;
+use axum::response::{IntoResponse, Redirect, Response};
+use axum::routing::{get, post};
 use maud::html;
 use serde::Deserialize;
-use sqlx::{query, query_scalar};
+use sqlx::{query, query_as, query_scalar};
 
+use super::new::problem_form;
 use super::pass_rate;
 use crate::error::ErrorResponse;
 use crate::extract::auth::User;
 use crate::extract::if_post::IfPost;
-use crate::model::{Language, ProblemId, SubmissionId, Tests};
+use crate::model::{Language, PermissionLevel, ProblemId, SubmissionId, Tests, UserId};
 use crate::template::{page, BannerKind};
 use crate::time::{now, Timestamp};
 use crate::util::{deserialize_textarea, s};
 use crate::{error, State};
-
-#[derive(Debug, Deserialize)]
-struct Post {
-	language: Language,
-	#[serde(deserialize_with = "deserialize_textarea")]
-	code: String,
-}
 
 fn can_edit(user: Option<&User>, problem_created_by: UserId) -> bool {
 	user.is_some_and(|user| {
 		user.permission_level >= PermissionLevel::Admin
 			|| (user.permission_level >= PermissionLevel::ProblemAuthor && user.id == problem_created_by)
 	})
+}
+
+async fn handle_edit_post(
+	state: &State,
+	problem_id: ProblemId,
+	post: &super::new::Problem,
+) -> Result<(), ErrorResponse> {
+	Tests::validate(&post.tests).map_err(|error| ErrorResponse::bad_request(error.to_string()))?;
+
+	query!("update problems set name = ?, description = ?, time_limit = ?, memory_limit = ?, visible = ?, tests = ? where id = ?", post.name, post.description, post.time_limit, post.memory_limit, post.visible, post.tests, problem_id).execute(&state.database).await.map_err(ErrorResponse::internal)?;
+
+	Ok(())
+}
+
+async fn edit_handler(
+	extract::State(state): extract::State<Arc<State>>,
+	user: Option<User>,
+	extract::Path(problem_id): extract::Path<ProblemId>,
+	IfPost(post): IfPost<extract::Form<super::new::Problem>>,
+) -> Result<Response, Response> {
+	let Some(created_by) = query_scalar!("select created_by from problems where id = ?", problem_id)
+		.fetch_optional(&state.database)
+		.await
+		.map_err(error::internal(user.as_ref()))?
+	else {
+		return Err(error::not_found(user.as_ref()).await);
+	};
+	if !can_edit(user.as_ref(), created_by) {
+		return Err(error::fake_not_found(user.as_ref()).await);
+	}
+
+	let post_res = if let Some(post) = post {
+		Some(handle_edit_post(&state, problem_id, &post).await)
+	} else {
+		None
+	};
+
+	let Some(problem) = query_as!(
+		super::new::Problem,
+		r#"select name, description, time_limit as "time_limit: u32", memory_limit as "memory_limit: u32", visible as "visible: bool", tests from problems inner join users on problems.created_by = users.id where problems.id = ?"#,
+		problem_id,
+	)
+	.fetch_optional(&state.database)
+	.await
+	.map_err(error::internal(user.as_ref()))?
+	else {
+		return Err(error::not_found(user.as_ref()).await);
+	};
+
+	let title = format!("Edit Problem {problem_id}");
+	let body = html! {
+		p { a href={"/problem/"(problem_id)} { "Back to problem page" } }
+		form method="post" {
+			(problem_form(Some(&problem)))
+			input type="submit" value="Edit";
+		}
+	};
+
+	let status = post_res
+		.as_ref()
+		.and_then(|res| res.as_ref().err())
+		.map_or(StatusCode::OK, |error| error.status);
+	let mut page = page(&title, user.as_ref(), &body);
+	page = match &post_res {
+		Some(Ok(())) => page.with_banner(BannerKind::Info, "Problem updated"),
+		Some(Err(error)) => page.with_banner(BannerKind::Error, &error.message),
+		None => page,
+	};
+	Ok((status, page).into_response())
+}
+
+async fn delete_handler(
+	extract::State(state): extract::State<Arc<State>>,
+	user: Option<User>,
+	extract::Path(problem_id): extract::Path<ProblemId>,
+) -> Result<Response, Response> {
+	let Some(created_by) = query_scalar!("select created_by from problems where id = ?", problem_id)
+		.fetch_optional(&state.database)
+		.await
+		.map_err(error::internal(user.as_ref()))?
+	else {
+		return Err(error::not_found(user.as_ref()).await);
+	};
+	if !can_edit(user.as_ref(), created_by) {
+		return Err(error::fake_not_found(user.as_ref()).await);
+	}
+
+	query!("delete from problems where id = ?", problem_id)
+		.execute(&state.database)
+		.await
+		.map_err(error::internal(user.as_ref()))?;
+
+	Ok(Redirect::to("/problems").into_response())
+}
+
+#[derive(Debug, Deserialize)]
+struct Post {
+	language: Language,
+	#[serde(deserialize_with = "deserialize_textarea")]
+	code: String,
 }
 
 async fn handle_post(
@@ -87,6 +181,12 @@ async fn handler(
 	let pass_rate = pass_rate(problem.num_submissions, problem.num_correct_submissions);
 
 	let body = html! {
+		@if can_edit(user.as_ref(), problem.created_by_id) {
+			div.row {
+				a href={"/problem/"(problem_id)"/edit"} { "Edit" }
+				form method="post" action={"/problem/"(problem_id)"/delete"} { input type="submit" value="Delete"; }
+			}
+		}
 		p { b {
 			"Created by " a href={"/users/"(problem.created_by_id)} { (problem.created_by_name) } " on " (problem.creation_time) " | " (problem.num_submissions) " submission" (s(problem.num_submissions))
 			@if let Some(pass_rate) = pass_rate {
@@ -128,5 +228,9 @@ async fn handler(
 }
 
 pub fn router() -> axum::Router<Arc<State>> {
-	axum::Router::new().route("/:id", get(handler).post(handler))
+	let router = axum::Router::new()
+		.route("/", get(handler).post(handler))
+		.route("/edit", get(edit_handler).post(edit_handler))
+		.route("/delete", post(delete_handler));
+	axum::Router::new().nest("/:id", router)
 }
