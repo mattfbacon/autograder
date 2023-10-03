f23b4a6e8e13d44ed06ce23ec178a82faaf12557
Matt Fellenz <matt@felle.nz>
2023-10-03 10:49:34 -0700

    Implement submission search

--- src/routes/admin.rs
+++ src/routes/admin.rs
@@ -8,30 +8,57 @@ use sqlx::{query, query_scalar};
 
 use crate::extract::auth::Admin;
 use crate::extract::pagination::RawPagination;
-use crate::model::{PermissionLevel, SimpleTestResponse};
+use crate::model::{PermissionLevel, ProblemId, SimpleTestResponse};
 use crate::template::page;
 use crate::time::Timestamp;
 use crate::{error, State};
 
 const DEFAULT_PAGE_SIZE: u32 = 30;
 
+#[derive(serde::Deserialize)]
+struct SubmissionsSearch {
+	submitter: Option<String>,
+	problem: Option<String>,
+	problem_id: Option<String>,
+}
+
 async fn submissions(
 	extract::State(state): extract::State<Arc<State>>,
 	Admin(user): Admin,
 	pagination: RawPagination,
+	extract::Query(search): extract::Query<SubmissionsSearch>,
 ) -> Result<Response, Response> {
+	let search_submitter = search.submitter.filter(|s| !s.is_empty());
+	let search_problem = search.problem.filter(|s| !s.is_empty());
+	let search_problem_id = search.problem_id.and_then(|s| s.parse::<ProblemId>().ok());
+	let any_search =
+		search_submitter.is_some() || search_problem.is_some() || search_problem_id.is_some();
+
 	let pagination = pagination.with_default_page_size(DEFAULT_PAGE_SIZE);
 	let limit = pagination.limit();
 	let offset = pagination.offset();
 
-	let num_submissions = query_scalar!(r#"select count(*) as "count: i64" from submissions"#)
+	let num_submissions = if any_search {
+		query_scalar!(r#"select count(*) as "count: i64" from submissions where (?1 is null or submissions.submitter in (select id from users where instr(display_name, ?1) > 0)) and (?2 is null or submissions.for_problem in (select id from problems where instr(name, ?2) > 0)) and (?3 is null or submissions.for_problem = ?3)"#, search_submitter, search_problem, search_problem_id)
+	} else {
+		query_scalar!(r#"select count(*) as "count: i64" from submissions"#)
+	}
 		.fetch_one(&state.database)
 		.await
 		.map_err(error::internal(Some(&user)))?;
 
-	let submissions = query!(r#"select submissions.id as submission_id, problems.id as problem_id, problems.name as problem_name, users.id as submitter_id, users.display_name as submitter_name, submission_time as "submission_time: Timestamp", result as "result: SimpleTestResponse" from submissions inner join problems on submissions.for_problem = problems.id inner join users on submissions.submitter = users.id order by submissions.id desc limit ? offset ?"#, limit, offset).fetch_all(&state.database).await.map_err(error::internal(Some(&user)))?;
+	let submissions = query!(r#"select submissions.id as submission_id, problems.id as problem_id, problems.name as problem_name, users.id as submitter_id, users.display_name as submitter_name, submission_time as "submission_time: Timestamp", result as "result: SimpleTestResponse" from submissions inner join problems on submissions.for_problem = problems.id inner join users on submissions.submitter = users.id where (?1 is null or submissions.submitter in (select id from users where instr(display_name, ?1) > 0)) and (?2 is null or submissions.for_problem in (select id from problems where instr(name, ?2) > 0)) and (?3 is null or submissions.for_problem = ?3) order by submissions.id desc limit ?4 offset ?5"#, search_submitter, search_problem, search_problem_id, limit, offset).fetch_all(&state.database).await.map_err(error::internal(Some(&user)))?;
 
 	let body = html! {
+		details open[any_search] {
+			summary { "Search" }
+			form method="get" {
+				label { "Submitter name" input type="text" name="submitter" value=[search_submitter.as_deref()]; }
+				label { "Problem name" input type="text" name="problem" value=[search_problem.as_deref()]; }
+				label { "Problem ID" input type="number" name="problem_id" value=[search_problem_id]; }
+				input type="submit" value="Search";
+			}
+		}
 		table {
 			thead { tr {
 				th { "ID" }
